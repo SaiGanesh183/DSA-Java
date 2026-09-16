727d36114823ec303baffa0ac0395c9211e5eea0
Sai Ganesh Kotti <saiganeshkotti@gmail.com>
2026-09-16 19:19:17 +0530

    Solution for subsets with duplicates

---
+++ Recursion/subsetswithduplicates.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class subsetswithduplicates {
+    public List<List<Integer>> rec(int [] nums, int in,List<List<Integer>> mainlist, List<Integer> list ) {
+        if(in>=nums.length) {
+            mainlist.add(new ArrayList<>(list));
+            return mainlist;
+        }
+        list.add(nums[in]);
+       mainlist= rec(nums, in+1, mainlist, list);
+        list.remove(list.size()-1);
+        while(in<nums.length-1 && nums[in+1]==nums[in]) {
+            in++;
+        }
+        mainlist=rec(nums, in+1, mainlist, list);
+        return mainlist;
+    
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+         List<List<Integer>> mainlist = new ArrayList<>();
+         List<Integer> list= new ArrayList<>();
+        mainlist=rec(nums,0,mainlist,list);
+        return mainlist;
+    }
+}
